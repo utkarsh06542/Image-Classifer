e1a5c9849fdeef81f1a5808134b7a5af9f07db5c
Utkarsh Prabhakar <arete.utkarsh@gmail.com>
2017-07-24 18:07:07 +0530

    Add files via upload

---
+++ image_prediction.py
@@ -0,0 +1,250 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+url = 'http://yaroslavvb.com/upload/notMNIST/'
+
+def maybe_download(filename, expected_bytes, force=False):
+  """Download a file if not present, and make sure it's the right size."""
+  if force or not os.path.exists(filename):
+    filename, _ = urlretrieve(url + filename, filename)
+  statinfo = os.stat(filename)
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified', filename)
+  else:
+    raise Exception(
+      'Failed to verify' + filename + '. Can you get to it with a browser?')
+  return filename
+
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+num_classes = 10
+np.random.seed(133)
+
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  return data_folders
+  
+train_folders = maybe_extract(train_filename)
+test_folders = maybe_extract(test_filename)
+
+#problem : Displaying images at random
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load_letter(folder, min_num_images):
+  """Load the data for a single letter label."""
+  image_files = os.listdir(folder)
+  dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+  image_index = 0
+  print(folder)
+  for image in os.listdir(folder):
+    image_file = os.path.join(folder, image)
+    try:
+      image_data = (ndimage.imread(image_file).astype(float) - 
+                    pixel_depth / 2) / pixel_depth
+      if image_data.shape != (image_size, image_size):
+        raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+      dataset[image_index, :, :] = image_data
+      image_index += 1
+    except IOError as e:
+      print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+    
+  num_images = image_index
+  dataset = dataset[0:num_images, :, :]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' %
+                    (num_images, min_num_images))
+    
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  return dataset
+        
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+  dataset_names = []
+  for folder in data_folders:
+    set_filename = folder + '.pickle'
+    dataset_names.append(set_filename)
+    if os.path.exists(set_filename) and not force:
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % set_filename)
+    else:
+      print('Pickling %s.' % set_filename)
+      dataset = load_letter(folder, min_num_images_per_class)
+      try:
+        with open(set_filename, 'wb') as f:
+          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+      except Exception as e:
+        print('Unable to save data to', set_filename, ':', e)
+  
+  return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+#problem 2 verify that data still looks good
+
+#problem 3 To verify that classes are balanced
+
+#convolution file starts
+
+pickle_file = 'notMNIST.pickle'
+
+with open(pickle_file, 'rb') as f:
+  save = pickle.load(f)
+  train_dataset = save['train_dataset']
+  train_labels = save['train_labels']
+  valid_dataset = save['valid_dataset']
+  valid_labels = save['valid_labels']
+  test_dataset = save['test_dataset']
+  test_labels = save['test_labels']
+  del save  # hint to help gc free up memory
+  print('Training set', train_dataset.shape, train_labels.shape)
+  print('Validation set', valid_dataset.shape, valid_labels.shape)
+  print('Test set', test_dataset.shape, test_labels.shape)
+
+  image_size = 28
+num_labels = 10
+num_channels = 1 # grayscale
+
+import numpy as np
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape(
+    (-1, image_size, image_size, num_channels)).astype(np.float32)
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
+
+batch_size = 16
+patch_size = 5
+depth = 16
+num_hidden = 64
+beta_regul = 1e-3
+drop_out = 0.5
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+  # Input data.
+  tf_train_dataset = tf.placeholder(
+    tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+  tf_valid_dataset = tf.constant(valid_dataset)
+  tf_test_dataset = tf.constant(test_dataset)
+  global_step = tf.Variable(0)
+  
+  # Variables.
+  layer1_weights = tf.Variable(tf.truncated_normal(
+      [patch_size, patch_size, num_channels, depth], stddev=0.1))
+  layer1_biases = tf.Variable(tf.zeros([depth]))
+  layer2_weights = tf.Variable(tf.truncated_normal(
+      [patch_size, patch_size, depth, depth], stddev=0.1))
+  layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
+  size3 = ((image_size - patch_size + 1) // 2 - patch_size + 1) // 2
+  layer3_weights = tf.Variable(tf.truncated_normal(
+      [size3 * size3 * depth, num_hidden], stddev=0.1))
+  layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+  layer4_weights = tf.Variable(tf.truncated_normal(
+      [num_hidden, num_hidden], stddev=0.1))
+  layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+  layer5_weights = tf.Variable(tf.truncated_normal(
+      [num_hidden, num_labels], stddev=0.1))
+  layer5_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+  
+  # Model.
+  def model(data, keep_prob):
+    # C1 input 28 x 28
+    conv1 = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='VALID')
+    bias1 = tf.nn.relu(conv1 + layer1_biases)
+    # S2 input 24 x 24
+    pool2 = tf.nn.avg_pool(bias1, [1, 2, 2, 1], [1, 2, 2, 1], padding='VALID')
+    # C3 input 12 x 12
+    conv3 = tf.nn.conv2d(pool2, layer2_weights, [1, 1, 1, 1], padding='VALID')
+    bias3 = tf.nn.relu(conv3 + layer2_biases)
+    # S4 input 8 x 8
+    pool4 = tf.nn.avg_pool(bias3, [1, 2, 2, 1], [1, 2, 2, 1], padding='VALID')
+    # F5 input 4 x 4
+    shape = pool4.get_shape().as_list()
+    reshape = tf.reshape(pool4, [shape[0], shape[1] * shape[2] * shape[3]])
+    hidden5 = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+    # F6
+    drop5 = tf.nn.dropout(hidden5, keep_prob)
+    hidden6 = tf.nn.relu(tf.matmul(hidden5, layer4_weights) + layer4_biases)
+    drop6 = tf.nn.dropout(hidden6, keep_prob)
+    return tf.matmul(drop6, layer5_weights) + layer5_biases
+  
+  # Training computation.
+  logits = model(tf_train_dataset, drop_out)
+  loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels,logits=logits))
+    
+  # Optimizer.
+  learning_rate = tf.train.exponential_decay(0.05, global_step, 1000, 0.85, staircase=True)
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+  
+  # Predictions for the training, validation, and test data.
+  train_prediction = tf.nn.softmax(logits)
+  valid_prediction = tf.nn.softmax(model(tf_valid_dataset, 1.0))
+  test_prediction = tf.nn.softmax(model(tf_test_dataset, 1.0))
+
+
+num_steps = 5001
+
+with tf.Session(graph=graph) as session:
+  tf.initialize_all_variables().run()
+  print('Initialized')
+  for step in range(num_steps):
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+    _, l, predictions = session.run(
+      [optimizer, loss, train_prediction], feed_dict=feed_dict)
+    if (step % 50 == 0):
+      print('Minibatch loss at step %d: %f' % (step, l))
+      print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+      print('Validation accuracy: %.1f%%' % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
